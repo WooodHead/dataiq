770786b752786f99cbe3c30e3a798d617f1ca2a2
abhijeetlokhande1996 <abhijeetlokhande1996@gmail.com>
2022-06-26 11:12:55 +0100

    look and fill of popup html changes

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ function update_mode(mode) {
 }
 function on_click_mode_button(reward_btn, privacy_btn, mode){
     if (mode == "reward"){
+        MODE = "reward"
         reward_btn.className="btn btn-primary"
         privacy_btn.className="btn btn-outline-primary"
         // Reward mode
@@ -17,12 +18,12 @@ function on_click_mode_button(reward_btn, privacy_btn, mode){
 
         
     } else if(mode == "privacy"){
-        MODE="privacy"
+        MODE = "privacy"
         privacy_btn.className="btn btn-primary"
         reward_btn.className="btn btn-outline-primary"
         // Call function for privacy mode
         // privacy mode is nothing but ad blocker
-        
+        update_mode("privacy")
                 
 
     }
@@ -34,13 +35,31 @@ function on_click_preferences(){
     window.open("https://www.google.com/")
     return;
 }
+function toggle_enable_dataiq_button(){
+    enable_dataiq_button_flag = MODE == "reward" ? true : false;
+    if(enable_dataiq_button_flag) {
+        enable_dataIQ.className = button_classname_and_state_mapping["enable_dataiq_button"]["active"]
+    } else if (!enable_dataiq_button_flag) {
+        enable_dataIQ.className = button_classname_and_state_mapping["enable_dataiq_button"]["deactive"]
+    }
+}
+MODE = "reward";
+let reward_btn=document.getElementById("reward-btn")
+let privacy_btn=document.getElementById("privacy-btn")
+let get_google_search_btn=document.getElementById("get-google-search")
+let enable_dataIQ = document.getElementById("btn-enable-dataiq");
+let enable_dataiq_button_flag = null;
+let button_classname_and_state_mapping = {
+    "enable_dataiq_button": {
+        "active": "btn btn-primary btn-lg btn-block",
+        "deactive": "btn btn-warning btn-lg btn-block"
+    }
+}
 
-reward_btn=document.getElementById("reward-btn")
-privacy_btn=document.getElementById("privacy-btn")
-get_google_search_btn=document.getElementById("get-google-search")
-preferences_btn=document.getElementById("btn-preferences")
-show_points_btn=document.getElementById("btn-show-points")
+// preferences_btn=document.getElementById("btn-preferences")
+// show_points_btn=document.getElementById("btn-show-points")
 
+/*
 show_points_btn.addEventListener("click", function(){
     // search_term_array
     chrome.storage.sync.get(["search_term_array"], function(resp){
@@ -49,6 +68,7 @@ show_points_btn.addEventListener("click", function(){
         document.getElementById("point-span").innerText = points
     })
 })
+
 preferences_btn.addEventListener("click", function(){
     on_click_preferences()
 })
@@ -60,15 +80,35 @@ get_google_search_btn.addEventListener("click", function (){
         alert(JSON.stringify(result))
     })
 })
+*/
 reward_btn.addEventListener("click", function(){
     on_click_mode_button(reward_btn, privacy_btn, "reward")
+    toggle_enable_dataiq_button()
+    
 })
 privacy_btn.addEventListener("click", function(){
     on_click_mode_button(reward_btn, privacy_btn, "privacy")
+    toggle_enable_dataiq_button()
 })
+enable_dataIQ.addEventListener("click", function(){
+    toggle_enable_dataiq_button()
+})
+
 chrome.storage.sync.get(["mode"], function(mode_dict){
+    mode_dict = mode_dict =! null ? mode_dict : {}
     if(!mode_dict["mode"]){
         update_mode("reward")
+    }
+    if(mode_dict["mode"]=="reward") {
+        enable_dataiq_button_flag = true;
+        enable_dataIQ.className = button_classname_and_state_mapping["enable_dataiq_button"]["active"]
+        on_click_mode_button(reward_btn, privacy_btn, "active")
+    } else if (mode_dict["mode"] == "privacy") {
+        enable_dataiq_button_flag = false;       
+        enable_dataIQ.className = button_classname_and_state_mapping["enable_dataiq_button"]["deactive"]
+        on_click_mode_button(reward_btn, privacy_btn, "privacy")
+    } else{
+        ;
     }
 })
 
