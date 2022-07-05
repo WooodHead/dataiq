f81af60462f7436643f93aa0408ade5354b38b36
abhijeetlokhande1996 <abhijeetlokhande1996@gmail.com>
2022-07-05 11:09:34 +0100

    new rules.json added

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 from db_conns import MongoDb
 
@@ -51,4 +52,7 @@ async def save_user_data(request: Request):
     return {
         "error": False
     }
+if __name__ == "__main__":
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
+
     
